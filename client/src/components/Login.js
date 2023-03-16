a1983e4f45f51157fd7439d26782363947b1a3de
Jessica <andradejsscl@gmail.com>
2023-03-16 16:37:49 -0400

    signup page styled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -41,10 +41,10 @@ function Login({updateUser}) {
     }
 
     return (
-        <div id="login-container"> 
+        <div className="login-container"> 
         <h1 className="h2">Welcome to ClubFlatiron</h1>
-            <div id="login-form-container">
-                <form id="login-form" onSubmit={onSubmit}>
+            <div className="login-form-container">
+                <form className="login-form" onSubmit={onSubmit}>
                     <label for="username">Username: </label>
                     <input type='text' name='username' value={username} onChange={handleChange} />
                     <br/><br/>
@@ -63,4 +63,4 @@ function Login({updateUser}) {
     )
 }
 
-export default Login;
+export default Login
