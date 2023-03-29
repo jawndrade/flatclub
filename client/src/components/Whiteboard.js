0c4a951c88da7657f8ff24e2a5451b956f2d63c8
Jessica <andradejsscl@gmail.com>
2023-03-29 14:07:36 -0400

    added whiteboard

---
+++ client/src/components/Whiteboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PencilTool from './PencilTool'
+
+function Whiteboard() {
+    return (
+      <div id='whiteboard-content'>
+        <PencilTool />
+      </div>
+    )
+  }
+  
+  export default Whiteboard
